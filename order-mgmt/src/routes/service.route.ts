7bc5647823042609315b6f4fb5c1e7501cb39fec
Arka Patra <arkapatra2009@gmail.com>
2024-11-26 18:36:08 +0530

    Update filterOrdersByCustomField to accept limit parameter and return order details

--- order-mgmt/src/routes/service.route.ts
+++ order-mgmt/src/routes/service.route.ts
@@ -1,13 +1,13 @@
 import { Router, Request, Response } from 'express';
 import { filterOrdersByCustomField } from '../service';
-import { OrderPagedSearchResponse } from '@commercetools/platform-sdk';
+import { OrderPagedQueryResponse } from '@commercetools/platform-sdk';
 
 const serviceRouter = Router();
 
 serviceRouter.use('/', async (req: Request, res: Response) => {
   try {
-    const { searchTerm } = req.body;
-    const orders: OrderPagedSearchResponse = await filterOrdersByCustomField(searchTerm);
+    const { searchTerm, limit } = req.body;
+    const orders: OrderPagedQueryResponse = await filterOrdersByCustomField(searchTerm, limit);
     res.status(200).send(orders);
   } catch (error) {
     console.error(error);
--- order-mgmt/src/service.ts
+++ order-mgmt/src/service.ts
@@ -1,7 +1,7 @@
-import { OrderPagedSearchResponse } from "@commercetools/platform-sdk";
+import { OrderPagedQueryResponse } from "@commercetools/platform-sdk";
 import { apiRoot, projectKey } from "./client/apiClient";
 
-export async function filterOrdersByCustomField(searchTerm: string): Promise<OrderPagedSearchResponse> {
+export async function filterOrdersByCustomField(searchTerm: string, limit: number): Promise<OrderPagedQueryResponse> {
     const orders = (await apiRoot.withProjectKey({ projectKey }).orders().search().post(
         {
             body: {
@@ -12,10 +12,31 @@ export async function filterOrdersByCustomField(searchTerm: string): Promise<Ord
                         "customType": "StringType",
                         "mustMatch": "any"
                     }
-                }
+                },
+                limit
             }
         }
     ).execute()).body;
 
-    return orders;
+    //Fetch the order IDs from the list of hits from orders in async manner
+    const orderIds = await Promise.all(orders.hits.map(async (order) => order.id));
+
+    const orderDetails = await showOrderDetails(orderIds);
+
+    return orderDetails;
+}
+
+async function showOrderDetails(orderIds: string[]): Promise<OrderPagedQueryResponse> {
+
+    const whereClause: string = orderIds.length === 1
+        ? `id in ("${orderIds[0]}")`
+        : `id in (${orderIds.map((id) => `"${id}"`).join(",")})`;
+
+
+    return (await apiRoot.withProjectKey({ projectKey }).orders().get({
+        queryArgs: {
+            expand: ["lineItems[*]"],
+            where: whereClause
+        }
+    }).execute()).body;
 }
