d4379c4c999c0ea84cd98160be2010b82ce66648
Arka Patra <arkapatra2009@gmail.com>
2024-11-26 15:29:34 +0530

    Refactor express app configuration and update service route

--- order-mgmt/src/app.ts
+++ order-mgmt/src/app.ts
@@ -1,21 +1,17 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-
 import express, { Express } from 'express';
-import bodyParser from 'body-parser';
 
 // Import routes
 import ServiceRoutes from './routes/service.route';
 
 // Create the express app
 const app: Express = express();
-app.disable('x-powered-by');
 
 // Define configurations
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Define routes
-app.post('/service', ServiceRoutes);
+app.post('/', ServiceRoutes);
 
 export default app;
